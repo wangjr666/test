5d8da16a8dc2999c660d9c03dc6c42a66852d3d8
吃橘子的匹诺曹 <45928390+wangjr666@users.noreply.github.com>
2020-08-04 13:44:03 +0800

    新增文件
    新增了一个java文件

---
+++ NewJava.java
@@ -0,0 +1,11 @@
+public class QueryVo {
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
