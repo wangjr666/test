fc76c560a52436ea8cf5d0641649804c9020b5dd
吃橘子的匹诺曹 <45928390+wangjr666@users.noreply.github.com>
2020-08-09 21:08:38 +0800

    Update NewJava.java

--- NewJava.java
+++ NewJava.java
@@ -1,5 +1,7 @@
 public class NewJava {
     public static void main(String[] args) {
         System.out.println("Test");
+        System.out.println("Test1");
+        System.out.println("Test2");
     }
 }
